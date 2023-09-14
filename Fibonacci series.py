b665e34fe8d31e514548324a9e883680433b672c
Aayush bijalwan <101926574+Aayushbijalwan16@users.noreply.github.com>
2023-09-15 00:57:47 +0530

    Update Fibonacci series.py

--- Fibonacci series.py
+++ Fibonacci series.py
@@ -1,11 +1,16 @@
-import sys
-n = len(sys.argv)
-print("Total arguments passed: ", n)
-print("\nName of Python script:", sys.argv[0])
-print("\nArguments passed:", end = " ")
-for i in range(1, n):
-	print(sys.argv[i], end = " ")
-Sum = 0
-for i in range(1, n):
-	Sum =Sum + int(sys.argv[i])
-print("\n\nResult:", Sum)
+nterms = int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
